128f0a4f29d6a6ed289f5fcac84ba11bfad7bd20
JoanR17 <joanr.m.1999@gmail.com>
2022-01-12 10:57:21 +0100

    Ejercicio 8

---
+++ Ejercicio 8/src/Ej8App.java
@@ -0,0 +1,22 @@
+
+public class Ej8App {
+
+	public static void main(String[] args) {
+
+		System.out.print("Numeros del 1 al 100 : ");
+		
+		for (int i = 1; i <= 100; i++)
+		{
+			if (i == 100)
+			{
+				System.out.print(i+".");
+			}
+			else
+			{
+				System.out.print(i+", ");
+			}
+		}
+		
+	}
+
+}
