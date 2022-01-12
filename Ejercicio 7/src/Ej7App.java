36e0e4a65f8d312b7cb569d18a51135d64c9d5bf
JoanR17 <joanr.m.1999@gmail.com>
2022-01-12 10:54:32 +0100

    Ejercicio 7

---
+++ Ejercicio 7/src/Ej7App.java
@@ -0,0 +1,25 @@
+
+public class Ej7App {
+
+	public static void main(String[] args) {
+
+		System.out.print("Numeros del 1 al 100 : ");
+		int i = 0;
+		
+		while (i < 100)
+		{
+			i++;
+			if (i == 100)
+			{
+				System.out.print(i+".");
+			}
+			else
+			{
+				System.out.print(i+", ");
+			}
+		}
+		
+
+	}
+
+}
