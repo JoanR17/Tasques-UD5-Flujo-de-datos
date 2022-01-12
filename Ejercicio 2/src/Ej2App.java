fe1bd5f1ee0f090c65d9cdf7ef9d8ea0d71b320b
JoanR17 <joanr.m.1999@gmail.com>
2022-01-12 09:14:48 +0100

    Ejercicio 2

---
+++ Ejercicio 2/src/Ej2App.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Ej2App {
+
+	public static void main(String[] args) {
+
+		String nom = "Joan";
+		
+		JOptionPane.showMessageDialog(null, "Bienvenido " + nom);
+
+	}
+
+}
