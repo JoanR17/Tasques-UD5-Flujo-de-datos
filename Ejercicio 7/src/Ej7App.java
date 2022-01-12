695d40b3995d63c425834ecbca512bfec36024e3
JoanR17 <joanr.m.1999@gmail.com>
2022-01-12 10:58:30 +0100

    Reedit

--- Ejercicio 7/src/Ej7App.java
+++ Ejercicio 7/src/Ej7App.java
@@ -3,7 +3,7 @@ public class Ej7App {
 
 	public static void main(String[] args) {
 
-		System.out.print("Numeros del 1 al 100 : ");
+		System.out.print("Numeros del 1 al 100: ");
 		int i = 0;
 		
 		while (i < 100)
--- Ejercicio 8/src/Ej8App.java
+++ Ejercicio 8/src/Ej8App.java
@@ -3,7 +3,7 @@ public class Ej8App {
 
 	public static void main(String[] args) {
 
-		System.out.print("Numeros del 1 al 100 : ");
+		System.out.print("Numeros del 1 al 100: ");
 		
 		for (int i = 1; i <= 100; i++)
 		{
